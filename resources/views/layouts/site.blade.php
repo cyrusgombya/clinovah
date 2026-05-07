c1c134d04da77ab70d916cd2835d68d290e01b0f
cyrusgombya <cyrusgombya@gmail.com>
2026-05-07 11:23:01 +0300

    Updated clinic pages with new UI

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,10 +4,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="@yield('meta_description', 'PHYSIC')">
-    <meta name="keywords" content="@yield('meta_keywords', 'PHYSIC')">
+    <meta name="description" content="@yield('meta_description', 'Clinovah')">
+    <meta name="keywords" content="@yield('meta_keywords', 'Clinovah')">
     <meta name="author" content="pixelstrap">
-    <title>@yield('title', 'PHYSIC - Premium Doctor Template')</title>
+    <title>@yield('title', 'Clinovah')</title>
     <link rel="icon" href="{{ asset('assets/clin/images/fav-icon.png') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
